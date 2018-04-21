f9a5e8bcf7783ef85f28ffabbb9d28bbf5377d43
Alma Niu <aniu@scu.edu>
2018-04-21 15:42:38 -0700

    Update load_data.php

--- load_data.php
+++ load_data.php
@@ -1,2 +1,17 @@
-# Justdoit2.github.io
-test
+<?php
+    $conn = oci_connect('aniu@db11g','Damnright1','//dbserver.engr.scu.edu/db11g');
+    if($conn) {
+        print "<br> Success.";
+    }
+    else {
+        print "<br> Failed.";
+        exit;
+    }
+    echo "<BR/>";
+    
+        $sql = "select * from people";
+        $stid = oci_parse($conn, $sql);
+        oci_execute($stid);
+        oci_free_statement($stid);
+        oci_close($conn);
+?>
