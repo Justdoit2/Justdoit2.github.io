ed2933b29bcf004f6109d8d12f627310ef41ef65
Alma Niu <aniu@scu.edu>
2018-04-21 16:18:33 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <p> welcome </p>
+  <?php
+    if(isset($_REQUEST['btn'])) {
+    $conn = oci_connect('aniu@db11g','Damnright1','//dbserver.engr.scu.edu/db11g');
+    if($conn) {
+        print "<br> Success.";
+    }
+    else {
+        print "<br> Failed.";
+        exit;
+    }
+    echo "<BR/>";
+    
+        $sql = "select * from people";
+        $stid = oci_parse($conn, $sql);
+        oci_execute($stid);
+        oci_free_statement($stid);
+        oci_close($conn);
+    }
+       
+?>
+</body>
+</html
